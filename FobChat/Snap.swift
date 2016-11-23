cebebc7e83d5b267cafe5f43551f688d94eac5da
EMMANUEL J ZAFRA <ejayzee@mac.com>
2016-11-22 23:45:55 -0800

    Pulling Snaps from Firebase

---
+++ FobChat/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  snaps.swift
+//  FobChat
+//
+//  Created by EMMANUEL J ZAFRA on 11/22/16.
+//  Copyright © 2016 EMMANUEL J ZAFRA. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var descrip = ""
+    var from = ""
+}
--- FobChat/SnapsViewController.swift
+++ FobChat/SnapsViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+       var snaps : [Snap] = []
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        FIRDatabase.database().reference().child("users").child(FIRAuth.auth()!.currentUser!.uid).child("snaps").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            let snap = Snap()
+            snap.imageURL = (snapshot.value! as! NSDictionary)["imageURL"] as! String
+            snap.from = (snapshot.value! as! NSDictionary)["from"] as! String
+            snap.descrip = (snapshot.value! as! NSDictionary)["description"] as! String
+            self.snaps.append(snap)
+            self.tableView.reloadData()
+        })
+
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let snap = snaps[indexPath.row]
+        cell.textLabel!.text = snap.from
+        return cell
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
